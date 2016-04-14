83f1cd03451b5ab9e1ead677aa1522b18e9b0441
ronrest <ronny.coding@gmail.com>
2016-04-14 16:26:29 +1000

    Random_Agent - Add Docstrings and Comments to Random_Agent

--- agents/Random_Agent.py
+++ agents/Random_Agent.py
@@ -3,10 +3,23 @@ __author__ = 'Ronny Restrepo'
 from Agent import Agent
 import random
 
+################################################################################
+#                                                                   RANDOM_AGENT
+################################################################################
 class Random_Agent(Agent):
+    """
+    An agent that when asked to make a choice of actions, always chooses one of
+    the available actions at random, using uniform distribution.
+    """
+    # ==========================================================================
+    #                                                                 __INIT__()
+    # ==========================================================================
     def __init__(self):
         Agent.__init__(self)
 
+    # ==========================================================================
+    #                                                             CHOSE_ACTION()
+    # ==========================================================================
     def chose_action(self, state, available_actions):
         return random.choice(available_actions)
 
