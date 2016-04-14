38ff7cca3718d4fae50bf8c09bf6e59e841320da
ronrest <ronny.coding@gmail.com>
2016-04-14 16:10:43 +1000

    Agent - Add comment lines to tidy things up

--- agents/Agent.py
+++ agents/Agent.py
@@ -19,24 +19,35 @@ class Agent(object):
         self.win_tally = {"win": 0, "lose": 0, "draw": 0}
         self.learning = True
 
-
     # ==========================================================================
-    #                                                             set_to_learn()
+    #                                                             SET_TO_LEARN()
     # ==========================================================================
     def set_to_learn(self, learn_state=True):
         self.learning = learn_state
 
+    # ==========================================================================
+    #                                                          RESET_WIN_TALLY()
+    # ==========================================================================
     def reset_win_tally(self):
         self.win_tally = {"win": 0, "lose": 0, "draw": 0}
 
+    # ==========================================================================
+    #                                                     RESET_STATES_ACTIONS()
+    # ==========================================================================
     def reset_states_actions(self):
         self.last_state = None
         self.state = None
         self.last_action = None
 
+    # ==========================================================================
+    #                                                             CHOSE_ACTION()
+    # ==========================================================================
     def chose_action(self, state):
         pass
 
+    # ==========================================================================
+    #                                                     ENVIRONMENT_FEEDBACK()
+    # ==========================================================================
     def environment_feedback(self, s1, a, s2, r=0):
         """
         takes a state, action, state transition with an optional reward
@@ -49,16 +60,22 @@ class Agent(object):
         self.state = s2
         self.last_state = s1
         self.last_action = a
-        #self.process_reward(r)
 
+    # ==========================================================================
+    #                                                              UPDATE_WINS()
+    # ==========================================================================
     def update_wins(self, win_status):
+        """ Updates the win tally"""
         self.win_tally[win_status] += 1
 
+    # ==========================================================================
+    #                                                           TERMINAL_STATE()
+    # ==========================================================================
     def terminal_state(self, s1, a, r):
         """
-        :param r:
         :param s1: last state
-        :param a: last action
+        :param a:  last action
+        :param r:  reward
         """
         pass
 
